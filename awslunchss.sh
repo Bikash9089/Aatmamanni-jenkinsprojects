018dd6231f97e4b6af95789fe24046ebaa2a9a05
Ubuntu <ubuntu@ip-172-31-2-176.us-east-2.compute.internal>
2022-04-13 12:57:24 +0000

    updated

--- awslunchss.sh
+++ awslunchss.sh
@@ -1,3 +1,7 @@
 #!/bin/bash/
 lunch=$(aws ec2 run-instances --image-id ami-064ff912f78e3e561 --count 1 --instance-type t2.micro --key-name AtmaniJobDemo --security-group-ids sg-00490ac282c9dba3a --subnet-id subnet-0c168018af026e075 --region us-east-2)
 echo $lunch
+stopt=$(aws ec2 stop-instances --instance-ids $InstanceID)
+echo $stopt
+startt=$(aws ec2 start-instances --instance-ids $InstanceID)
+echo $startt
