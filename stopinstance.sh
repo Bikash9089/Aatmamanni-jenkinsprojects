76200a9f60b88046d574c96bf5808c83a5eab011
Ubuntu <ubuntu@ip-172-31-2-176.us-east-2.compute.internal>
2022-04-13 13:34:13 +0000

    up

--- stopinstance.sh
+++ stopinstance.sh
@@ -1,4 +1,4 @@
 #!/bin/bash/
-Instance_ID="i-0bdad35bb5ae28d54"
-stopp=$(aws ec2 stop-instances --instance-ids $Instance_ID --region us-east-2)
+InstanceID="i-0bdad35bb5ae28d54"
+stopp=$(aws ec2 stop-instances --instance-ids $InstanceID --region us-east-2)
 echo $stopp
