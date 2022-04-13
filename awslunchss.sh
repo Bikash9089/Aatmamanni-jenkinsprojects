f2b2a6f32ff9981e4bfed6766c1bb0c9b778a171
Ubuntu <ubuntu@ip-172-31-2-176.us-east-2.compute.internal>
2022-04-13 06:10:21 +0000

    aws launch

---
+++ awslunchss.sh
@@ -0,0 +1,3 @@
+#!/bin/bash/
+lunch=$(aws ec2 run-instances --image-id ami-064ff912f78e3e561 --count 1 --instance-type t2.micro --key-name AtmaniJobDemo --security-group-ids sg-00490ac282c9dba3a --subnet-id subnet-0c168018af026e075)
+echo $lunch
