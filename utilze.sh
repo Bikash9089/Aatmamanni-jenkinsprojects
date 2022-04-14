af0a84232797d50fa14b52f5b14475fcfc1a316c
Ubuntu <ubuntu@ip-172-31-2-176.us-east-2.compute.internal>
2022-04-14 14:17:28 +0000

    up

--- utilze.sh
+++ utilze.sh
@@ -1,5 +1,4 @@
 #!/bin/bash/
 diskuti=$(df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }')
 echo $diskuti
-curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"${diskuti}\"}" https://hooks.slack.com/services/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
 
