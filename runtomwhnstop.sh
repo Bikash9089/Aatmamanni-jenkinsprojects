d0f0374623f200d52310e117275a285074cf4dff
Ubuntu <ubuntu@ip-172-31-2-176.us-east-2.compute.internal>
2022-04-15 08:31:12 +0000

    runtomcatwhenstoped

---
+++ runtomwhnstop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+status=$(netstat -na | grep 8090 | awk '{print $6}' | wc -l)
+if [ $status == "0" ]
+then
+	cd /home/ubuntu/assignments/shell3/apache-tomcat-8.5.78/bin
+	sh startup.sh
+fi
--- tomcatnotrunning.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash/
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
