45b21f4230a9ca74b8544d894d57060827ac72fd
Ubuntu <ubuntu@ip-172-31-2-176.us-east-2.compute.internal>
2022-04-16 15:11:33 +0000

    dsf

--- runtomwhnstop.sh
+++ runtomwhnstop.sh
@@ -2,5 +2,5 @@
 status=$(netstat -na | grep 8090 | awk '{print $6}' | wc -l)
 if [ $status == "0" ]
 then
-	sudo -S bash /home/ubuntu/assignments/shell3/apache-tomcat-8.5.78/bin/startup.sh
+	sudo bash /home/ubuntu/assignments/shell3/apache-tomcat-8.5.78/bin/startup.sh
 fi
